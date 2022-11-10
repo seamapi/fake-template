fbf4cbe484e332cd39a6fa6ca6676cb5704d8157
Severin Ibarluzea <seveibar@gmail.com>
2022-11-10 15:54:49 -0800

    add type and openapi generation

--- src/lib/types/thing.ts
+++ src/lib/types/thing.ts
@@ -1,11 +1,6 @@
-import { NextApiRequest } from "next"
 import { DatabaseState } from "lib/db"
 import { z } from "zod"
 import { Simplify } from "type-fest"
-
-export interface RequestWithDb extends NextApiRequest {
-  db: DatabaseState
-}
 
 export const thingZod = z.object({
   thing_id: z.string(),
