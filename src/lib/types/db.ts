b4c850cd2b0ef6316c0ff29d6a4423be4a53039a
andrii-balitskyi <10balian10@gmail.com>
2023-04-11 14:46:54 +0200

    Update db, fix bundle size job name

--- src/lib/types/db.ts
+++ src/lib/types/db.ts
@@ -1,4 +1,4 @@
-import { StoreApi } from "zustand/vanilla"
+import { StoreApi, createStore } from "zustand/vanilla"
 import { Thing, ThingInitializer } from "./"
 
 export interface DatabaseState {
@@ -14,3 +14,9 @@ export interface DatabaseMethods {
 
 export type Database = StoreApi<DatabaseState & DatabaseMethods> &
   DatabaseMethods
+
+export const createDatabaseStore = () =>
+  createStore<DatabaseState>((set, get) => ({
+    thingCount: 0,
+    things: [],
+  }))
