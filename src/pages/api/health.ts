2dea76eb74aaab72bb20730ecf8d572216d00f2a
andrii-balitskyi <10balian10@gmail.com>
2023-04-11 14:44:22 +0200

    Fix test, update nsm, nexlove, package lock file

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -5,9 +5,10 @@ export default withRouteSpec({
   auth: "none",
   methods: ["GET"],
   middlewares: [],
-  jsonResponse: z.object({ note: z.string() }),
+  jsonResponse: z.object({ note: z.string(), ok: z.boolean() }),
 } as const)(async (req, res) => {
   res.status(200).json({
+    ok: true,
     note: "this is not part of the [insert manufacturer name] api",
   })
 })
