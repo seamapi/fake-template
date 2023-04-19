8ae83966208753d4bbb08b20d2215fdf62e98c4b
Evan Sosenko <evan@getseam.com>
2023-04-18 20:51:52 -0700

    Pass type to axios in test

--- test/api/health.test.ts
+++ test/api/health.test.ts
@@ -1,15 +1,19 @@
 import test, { type ExecutionContext } from 'ava'
 
+import { type Routes } from 'index.ts'
+
 import {
   getTestServer,
   type SimpleAxiosError,
 } from 'fixtures/get-test-server.ts'
 
+type Route = Routes['/health']
+
 test('GET /health', async (t: ExecutionContext) => {
   const { axios } = await getTestServer(t)
-  const res = await axios.get('/health')
-  t.true(res.data.ok)
-  t.truthy(res.data.note)
+  const { data } = await axios.get<Route['jsonResponse']>('/health')
+  t.true(data.ok)
+  t.truthy(data.note)
 })
 
 // Example of testing API error.
