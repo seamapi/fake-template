20458ef22ab9dd6aabd66457e2dbe98ac140567f
Evan Sosenko <evan@getseam.com>
2023-07-19 21:14:09 -0700

    Remove default export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from "./route-types.ts"
-export { default } from "lib/index.ts"
 export * from "lib/index.ts"
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
 export type { Database } from "./database/index.ts"
 export { createDatabase, createSampleDatabase } from "./database/index.ts"
-export { createFake as create, createFake, Fake } from "./fake.ts"
-export { createFake as default } from "./fake.ts"
+export { createFake, Fake } from "./fake.ts"
 export * from "./models.ts"
 export { startServer } from "./server.ts"
