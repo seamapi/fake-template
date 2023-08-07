e8570e86cdaad3f3ef095d93f134578fd8155e45
Evan Sosenko <evan@getseam.com>
2023-08-07 15:20:29 -0700

    Simplify tsup banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,21 +2,19 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   tsconfig: "tsconfig.build.json",
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/server.ts"],
   format: "esm",
   treeshake: true,
   dts: true,
   sourcemap: true,
-  clean: true,
-  // UPSTREAM: https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
+  // UPSTREAM: Required as nsm imported code relies on __filename  and __dirname.
+  shims: true,
+  // UPSTREAM: Required as imported code relies on require.
+  // https://github.com/evanw/esbuild/issues/1921#issuecomment-1623640043
   banner: {
     js: `
-      import { fileURLToPath } from 'node:url';
-      import { createRequire as topLevelCreateRequire } from 'node:module';
-      import path from 'node:path'
-      const require = topLevelCreateRequire(import.meta.url);
-      const __filename = fileURLToPath(import.meta.url);
-      const __dirname = path.dirname(__filename);
+    import { createRequire as topLevelCreateRequire } from 'module';
+    const require = topLevelCreateRequire(import.meta.url);
     `,
   },
 })
